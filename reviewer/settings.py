cd9b601b73dac7ea0c50f914edd9aa376b7b1455
Eric Wang <ezwang1@gmail.com>
2017-12-23 18:18:33 -0500

    add auth and reviews apps

--- reviewer/settings.py
+++ reviewer/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'reviewer.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "reviewer", "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
