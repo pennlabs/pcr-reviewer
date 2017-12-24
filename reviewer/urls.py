9f209098a24ce25bbebc646272abe8a90285a261
Eric Wang <ezwang1@gmail.com>
2017-12-23 19:41:02 -0500

    customize admin

--- reviewer/urls.py
+++ reviewer/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = "PCR Comment Reviewer Admin"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('reviewer.auth.urls')),
