4d3704bd20954a9684cf18b5c776cf3d98ce827d
Eric Wang <ezwang1@gmail.com>
2017-12-23 21:24:31 -0500

    add tag to admin interface

--- reviewer/reviews/admin.py
+++ reviewer/reviews/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Instructor, Comment, Review
+from .models import Instructor, Comment, Review, Tag
 
 admin.site.register(Instructor)
 admin.site.register(Comment)
 admin.site.register(Review)
+admin.site.register(Tag)
--- reviewer/reviews/models.py
+++ reviewer/reviews/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Tag(models.Model):
     name = models.TextField()
 
