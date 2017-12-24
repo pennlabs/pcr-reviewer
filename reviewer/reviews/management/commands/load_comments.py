52ec708d53a8e74b0bacc9de0f8ce0e4e98287ce
Eric Wang <ezwang1@gmail.com>
2017-12-24 14:43:27 -0500

    add additional output to script

--- reviewer/reviews/management/commands/load_comments.py
+++ reviewer/reviews/management/commands/load_comments.py
@@ -12,6 +12,7 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument("file", nargs="?", type=str)
+        parser.add_argument("--fake", action="store_true", dest="fake", help="Don't actually import anything into the database.")
 
     def handle(self, *args, **options):
         if options["file"]:
@@ -21,6 +22,11 @@ class Command(BaseCommand):
         if not os.path.isfile(filename):
             raise CommandError("The file '{}' does not exist!".format(filename))
 
+        fake = options["fake"]
+
+        if fake:
+            self.stdout.write(self.style.WARNING("Running in fake mode, no changes will be made to the database."))
+
         self.stdout.write("Parsing sql file...")
 
         with open(filename, "r") as f:
@@ -28,25 +34,43 @@ class Command(BaseCommand):
 
         self.stdout.write(self.style.SUCCESS("Finished parsing sql file!"))
 
+        instructors = set()
+        sections = set()
+        comments = 0
+
+        self.stdout.write("Importing records...")
+
         for statement in parsed:
             if statement.get_type() == "INSERT":
                 info = statement[-2][1]
                 info = [x for x in info if not (x.ttype == sqlparse.tokens.Whitespace or x.ttype == sqlparse.tokens.Punctuation)]
                 inst_id = int(str(info[0])[1:-1])
                 inst_name = str(info[1])[1:-1]
-                try:
-                    inst, _ = Instructor.objects.get_or_create(
-                        id=inst_id,
-                        name=inst_name
+                section = str(info[3])[1:-1]
+
+                instructors.add(inst_id)
+                sections.add(section)
+                comments += 1
+
+                if comments % 1000 == 0:
+                    self.stdout.write("Imported {} records...".format(comments))
+
+                if not fake:
+                    try:
+                        inst, _ = Instructor.objects.get_or_create(
+                            id=inst_id,
+                            name=inst_name
+                        )
+                    except IntegrityError:
+                        self.stdout.write(self.style.WARNING("Duplicate instructor object ({}, {}) with different names!").format(inst_id, inst_name))
+                        inst = Instructor.objects.get(id=inst_id)
+                    Comment.objects.create(
+                        instructor=inst,
+                        term=str(info[2])[1:-1],
+                        section=section,
+                        text=str(info[-1])[1:-1].replace("''", "'")
                     )
-                except IntegrityError:
-                    self.stdout.write(self.style.WARNING("Duplicate instructor object ({}, {}) with different names!").format(inst_id, inst_name))
-                    inst = Instructor.objects.get(id=inst_id)
-                Comment.objects.create(
-                    instructor=inst,
-                    term=str(info[2])[1:-1],
-                    section=str(info[3])[1:-1],
-                    text=str(info[-1])[1:-1].replace("''", "'")
-                )
 
         self.stdout.write(self.style.SUCCESS("Finished importing comment data!"))
+
+        self.stdout.write(self.style.SUCCESS("Total Instructors: {}, Total Sections: {}, Total Comments: {}".format(len(instructors), len(sections), comments)))
