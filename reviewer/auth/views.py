60eb3fd8212aaf61cab6f8269584b4fd9acad956
Eric Wang <ezwang1@gmail.com>
2017-12-23 19:36:41 -0500

    setup auth and messages

--- reviewer/auth/views.py
+++ reviewer/auth/views.py
@@ -1,9 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.views.decorators.http import require_http_methods
+from django.contrib import messages
 
 
 def index(request):
-    return render(request, "index.html")
+    if request.user.is_authenticated:
+        return render(request, "dashboard.html")
+    else:
+        return render(request, "index.html")
 
 
+@require_http_methods(["POST"])
 def login(request):
-    pass
+    username = request.POST.get("username")
+    password = request.POST.get("password")
+    user = authenticate(username=username, password=password)
+    if user is None:
+        messages.error(request, "Login attempt failed!")
+    return redirect("index")
