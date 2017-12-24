86e345570b6e8e422a63bfb5196a472beee78eba
Eric Wang <ezwang1@gmail.com>
2017-12-23 21:11:41 -0500

    add tag model

--- reviewer/reviews/models.py
+++ reviewer/reviews/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Tag(models.Model):
+    name = models.TextField()
+
 
 class Instructor(models.Model):
     id = models.IntegerField(primary_key=True)
@@ -17,3 +20,4 @@ class Comment(models.Model):
 class Review(models.Model):
     comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
     reviewer = models.ForeignKey(User, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag)
