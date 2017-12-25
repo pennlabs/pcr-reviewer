7c729cc4ce448e3b8a5ec641a1a7db4ef35c58b1
Eric Wang <ezwang1@gmail.com>
2017-12-25 17:30:15 -0500

    add section to admin

--- reviewer/reviews/admin.py
+++ reviewer/reviews/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Instructor, Comment, Review, Tag, CommentRating
+from .models import Instructor, Comment, Review, Tag, CommentRating, Section
 
 admin.site.register(Instructor)
 admin.site.register(Comment)
 admin.site.register(Review)
 admin.site.register(Tag)
 admin.site.register(CommentRating)
+admin.site.register(Section)
