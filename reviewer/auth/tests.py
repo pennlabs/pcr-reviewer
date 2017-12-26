f2f5f12008f0ca1ebc302fceb43a201326699d14
Eric Wang <ezwang1@gmail.com>
2017-12-25 21:56:12 -0500

    set login url

--- reviewer/auth/tests.py
+++ reviewer/auth/tests.py
@@ -8,3 +8,7 @@ class BasicTestCase(TestCase):
     def test_index(self):
         resp = self.client.get('/')
         self.assertEqual(resp.status_code, 200)
+
+    def test_review_without_auth(self):
+        resp = self.client.get('/review')
+        self.assertEqual(resp.status_code, 302)
--- reviewer/settings.py
+++ reviewer/settings.py
@@ -108,6 +108,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+LOGIN_URL = "/"
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
