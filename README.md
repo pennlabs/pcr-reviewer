d57d446a7071f8c439d3eac336e789aa0d05efef
Eric Wang <ezwang1@gmail.com>
2017-12-25 22:15:59 -0500

    shorten titles

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# PCR Comment Reviewer
+# Comment Reviewer
 
 [![Build Status](https://travis-ci.org/pennlabs/pcr-reviewer.svg?branch=master)](https://travis-ci.org/pennlabs/pcr-reviewer)
 
--- reviewer/urls.py
+++ reviewer/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-admin.site.site_header = "PCR Comment Reviewer Admin"
+admin.site.site_header = "Comment Reviewer Admin"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
