e1d20f5f8cbff4510017a2004694063830c0e96b
Daniel Tao <danieltaox@gmail.com>
2019-04-14 14:56:06 -0400

    make flag buttons submit buttons

--- reviewer/static/js/review.js
+++ reviewer/static/js/review.js
@@ -18,38 +18,4 @@ $(document).ready(function() {
             });
         }
     });
-    $(".inappropriate").click(function(e) {
-        e.preventDefault();
-        if ($(this).attr("value") != "true") {
-            $(this).text("Flagged").attr("value", "true");
-        }
-        else {
-            $(this).text("Flag as Inappropriate").attr("value", "false");
-        }
-    });
-    $(".mark").click(function(e) {
-        e.preventDefault();
-        if ($(this).attr("value") != "true") {
-            $(this).text("Marked").attr("value", "true");
-        }
-        else {
-            $(this).text("Mark as Not Useful").attr("value", "false");
-        }
-    });
-    $(".approve").click(function(e) {
-        e.preventDefault();
-        if ($(this).attr("value") != "true") {
-            $(this).text("Approved").attr("value", "true");
-        }
-        else {
-            $(this).text("Approve Comment").attr("value", "false");
-        }
-    });
-    $("form").submit(function(e) {
-        $("#order").val(items.filter(rank => rank !== null).reverse().join());
-        var mark = $(this).find(".mark").attr("value");
-        var inap = $(this).find(".inappropriate").attr("value");
-        var appr = $(this).find(".approve").attr("value");
-        $(this).find("input[name=flag]").val(inap == "true" ? "I" : (mark == "true" ?  "M" : (appr == "true" ? "A" : "")));
-    });
 });
