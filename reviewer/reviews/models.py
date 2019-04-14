121373394e72ca02249903d617f30fffdb5fad47
Daniel Tao <danieltaox@gmail.com>
2019-04-14 15:28:15 -0400

    remove temporary migration settings

--- reviewer/reviews/models.py
+++ reviewer/reviews/models.py
@@ -35,7 +35,7 @@ class Comment(models.Model):
 
 
 class Review(models.Model):
-    comment = models.ForeignKey(Comment, on_delete=models.CASCADE, default=1)
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
     section = models.ForeignKey(Section, on_delete=models.CASCADE)
     reviewer = models.ForeignKey(User, on_delete=models.CASCADE)
     tags = models.ManyToManyField(Tag)
