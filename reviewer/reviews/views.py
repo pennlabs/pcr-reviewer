59036bd63e902ddadb8f024dbe692a5eb737cf4e
Daniel Tao <danieltaox@gmail.com>
2019-04-13 23:31:49 -0400

    update views.py to remove CommentRating object references

--- reviewer/reviews/views.py
+++ reviewer/reviews/views.py
@@ -14,49 +14,26 @@ from .helpers import get_next_section, select_random_comments
 def review(request):
     if request.method == "POST":
         section = get_object_or_404(Section, id=request.POST.get("section"))
-
         if Review.objects.filter(section=section, reviewer=request.user).exists():
             messages.error(request, "You have already reviewed this class!")
             return redirect("review")
 
-        order = [x.strip() for x in request.POST.get("order").split(",")]
-        order = [get_object_or_404(Comment, id=x) for x in order if x]
-
+        comment = get_object_or_404(Comment, id=request.POST.get("comment")
         tags = [x.strip() for x in request.POST.get("tags", "").split(",")]
         tags = [x.lower() for x in tags if x]
+        flag = request.POST.get("flag")
 
-        try:
-            flags = [(int(x.split("_")[-1]), request.POST.get(x)) for x in request.POST if x.startswith("flags_")]
-            flags = [x for x in flags if x[1]]
-        except ValueError:
-            messages.error(request, "Errors occured while parsing flags!")
-            return redirect("review")
-
-        flags = {x: y.upper()[0] for x, y in flags}
-
-        if len(order) >= 1:
-            review = Review.objects.create(
-                section=section,
-                reviewer=request.user
+        review = Review.objects.create(
+            comment=comment,
+            section=section,
+            reviewer=request.user,
+            flag=flag
+        )
+        for tag in tags:
+            tag, _ = Tag.objects.get_or_create(
+                name=tag
             )
-
-            for tag in tags:
-                tag, _ = Tag.objects.get_or_create(
-                    name=tag
-                )
-                review.tags.add(tag)
-
-            for i in range(len(order)):
-                CommentRating.objects.create(
-                    review=review,
-                    comment=order[i],
-                    rating=i,
-                    flag=flags.get(order[i].id)
-                )
-
-            messages.success(request, "Your review has been added!")
-        else:
-            messages.error(request, "You did not submit any comments for review!")
+        review.tags.add(tag)
         return redirect("review")
 
     section = get_next_section(request.user)
@@ -92,7 +69,7 @@ def stats(request):
     semesters = Section.objects.values("term").distinct().order_by("-term").values_list("term", flat=True)
     comments = {x["section__term"]: x["count"] for x in Comment.objects.values("section__term").annotate(count=Count("section__term"))}
     classes = {x["term"]: x["count"] for x in Section.objects.values("term").annotate(count=Count("term"))}
-    reviews = {x["term"]: x["count"] for x in Section.objects.values("term").annotate(count=Count("comment__commentrating__review", distinct=True))}
+    reviews = {x["term"]: x["count"] for x in Section.objects.values("term").annotate(count=Count("comment__review", distinct=True))}
     leaderboard = User.objects.annotate(reviews=Count("review")).order_by("reviews")[:10]
 
     for semester in semesters:
